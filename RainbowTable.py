7f4b1ed24cf9f52b66649c75152d0a9cb869d6b2
Jialin Ding <djialin09@gmail.com>
2014-11-06 21:41:58 -0800

    Added functions, haven't tested them yet

--- RainbowTable.py
+++ RainbowTable.py
@@ -4,15 +4,48 @@ import hashlib
 import random
 import string
 
-rainbowTable = {}
-for _ in range(3000):
-    start = ""
-    for _ in range(6):
-        start += random.choice(string.ascii_lowercase)
-    plainText = start
-    for _ in range(100000):
+def createRainbowTable():
+    rainbowTable = {}
+    for _ in range(3000):
+        start = ""
+        for _ in range(6):
+            start += random.choice(string.ascii_lowercase)
+        plainText = start
+        for _ in range(100000):
+            h = hashlib.sha256()
+            h.update(plainText)
+            hash = h.hexdigest()
+            plainText = hash[:6]
+        rainbowTable[start] = hash
+    return rainbowTable
+
+def expandRainbowTable(rainbowTable):
+    for _ in range(500):
+        start = ""
+        for _ in range(6):
+            start += random.choice(string.ascii_lowercase)
+        plainText = start
+        for _ in range(100000):
+            h = hashlib.sha256()
+            h.update(plainText)
+            hash = h.hexdigest()
+            plainText = hash[:6]
+        rainbowTable[start] = hash
+    return rainbowTable
+
+def getPassword(hashedPassword, rainbowTable):
+    while True:
+        for start in rainbowTable:
+            if rainbowTable[start] == hashedPassword:
+                return traverseChain(hashedPassword, rainbowTable, start)
         h = hashlib.sha256()
-        h.update(plainText)
-        hash = h.hexdigest()
-        plainText = hash[:6]
-    rainbowTable[start] = hash
+        h.update(hashedPassword[:6])
+        hashedPassword = h.hexdigest()
+
+def traverseChain(hashedPassword, start):
+    while True:
+        h = hashlib.sha256()
+        h.update(start)
+        if h.hexdigest() == hashedPassword:
+            return start
+        start = h.hexdigest()[:6]
