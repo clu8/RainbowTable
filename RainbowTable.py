8fb56405fc06ae1d1f08f6774d6b4eee9833b485
Charles Lu <charleslu@live.com>
2014-12-03 16:56:47 -0800

    Use pickle for faster and smaller table

--- RainbowTable.py
+++ RainbowTable.py
@@ -14,15 +14,17 @@ import hashlib
 import random
 import string
 import csv
+import pickle
+import os
 import time
 
 CHAIN_LENGTH = 1000
 ROWS = 3 * 10**6
-TABLE_FILE = "RainbowTable.csv"
-TABLE_FIELDNAMES = ['start_point', 'endpoint_hash']
-rainbowTable = {}
+CSV_FILE = "RainbowTable.csv"
+CSV_FIELDNAMES = ['start_point', 'endpoint_hash']
+PICKLE_FILE = "RainbowTable.pickle"
 
-"""Creates rainbow table using H() and R() with ROWS of CHAIN_LENGTH in TABLE_FILE
+"""Creates rainbow table using H() and R() with ROWS of CHAIN_LENGTH in CSV_FILE
 Precondition: To expand, input number of rows as param expand
 """
 def create_rainbow_table(expandRows=None):
@@ -39,8 +41,8 @@ def create_rainbow_table(expandRows=None):
         print("Creating rainbow table...")
 
     startTime = time.time()
-    with open(TABLE_FILE, mode) as table:
-        writer = csv.DictWriter(table, fieldnames=TABLE_FIELDNAMES)
+    with open(CSV_FILE, mode) as table:
+        writer = csv.DictWriter(table, fieldnames=CSV_FIELDNAMES)
         writer.writeheader()
         for i in range(rows):
             if i % 1000 == 0:
@@ -54,27 +56,36 @@ def create_rainbow_table(expandRows=None):
             for col in range(CHAIN_LENGTH):
                 hash = H(plainText)
                 plainText = R(hash, col)
-            writer.writerow({TABLE_FIELDNAMES[0]: start, TABLE_FIELDNAMES[1]: hash})
+            writer.writerow({CSV_FIELDNAMES[0]: start, CSV_FIELDNAMES[1]: hash})
 
     elapsed = time.time() - startTime
     print("Done in {0} mins, {1} secs.".format(int(elapsed / 60), elapsed % 60))
 
-'''Clears any existing rainbow table loaded into memory and reloads it from TABLE_FILE.
+'''Clears any existing rainbow table loaded into memory and reloads it from CSV_FILE.
+Also stores the dictionary in PICKLE_FILE for fast access.
 Keys are endpoint hashes, values are start plaintexts.
 '''
 def load_rainbow_table():
-    rainbowTable.clear()
-    print("Loading rainbow table...")
+    global RAINBOW_TABLE
+    RAINBOW_TABLE = {}
+    print("Loading rainbow table from CSV and saving as pickle file...")
     startTime = time.time()
-    with open(TABLE_FILE, 'r') as table:
+    with open(CSV_FILE, 'r') as table:
         reader = csv.DictReader(table)
         for row in reader:
-            rainbowTable[row[TABLE_FIELDNAMES[1]]] = row[TABLE_FIELDNAMES[0]]
+            RAINBOW_TABLE[row[CSV_FIELDNAMES[1]]] = row[CSV_FIELDNAMES[0]]
+
+    pickle.dump(RAINBOW_TABLE, open(PICKLE_FILE, "wb"))
+
     print("Done loading in {0} secs.".format(time.time() - startTime))
 
-def crack(hashedPassword):
-    if len(rainbowTable) == 0:
+def crack(hashedPassword, reloadTable=False):
+    global RAINBOW_TABLE
+    if reloadTable or not os.path.isfile(PICKLE_FILE):
         load_rainbow_table()
+    if reloadTable or not RAINBOW_TABLE:
+        print("Loading rainbow table from pickle file...")
+        RAINBOW_TABLE = pickle.load(open(PICKLE_FILE, "rb"))
 
     print("Cracking hash...")
     startTime = time.time()
@@ -82,8 +93,8 @@ def crack(hashedPassword):
     for col in range(CHAIN_LENGTH):
         candidate = hashedPassword
         for column in range(col, CHAIN_LENGTH):
-            if candidate in rainbowTable:
-                traversalResult = traverse_chain(hashedPassword, rainbowTable[candidate])
+            if candidate in RAINBOW_TABLE:
+                traversalResult = traverse_chain(hashedPassword, RAINBOW_TABLE[candidate])
                 if traversalResult:
                     print("Done cracking in {0} secs.".format(time.time() - startTime))
                     return traversalResult
@@ -135,6 +146,6 @@ def test(password=""):
 
     cracked = crack(H(password))
     if cracked:
-        print("Cracked password: {0}".format(crack(H(password))))
+        print("Cracked password: {0}".format(cracked))
     else:
         print("Unsuccessful :(")
